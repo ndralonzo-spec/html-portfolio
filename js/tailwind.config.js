e0494889dbf1241764cf3fc09f8de866917bb848
ndralonzo-spec <ndr.alonzo@gmail.com>
2026-03-23 09:05:36 -0400

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,31 @@
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "surface-bright": "#2b3a4f",
+                "surface-dim": "#041427",
+                "surface-container-high": "#1c2b3f",
+                "surface-container-low": "#0d1c30",
+                "surface-container": "#112034",
+                "surface-container-highest": "#27354a",
+                "surface-container-lowest": "#010e22",
+                "surface-variant": "#27354a",
+                "on-surface-variant": "#bfc8ce",
+                "on-surface": "#d4e3ff",
+                "outline": "#899297",
+                "outline-variant": "#3f484d",
+                "primary": "#85d0f3",
+                "primary-container": "#53a0c1",
+                "on-primary": "#003546",
+                "background": "#041427",
+                "tertiary": "#ffb872"
+            },
+            fontFamily: {
+                "headline": ["Manrope"],
+                "body": ["Inter"],
+                "label": ["Inter"]
+            }
+        }
+    }
+};
